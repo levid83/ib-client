dbc4076f6e8a5a607deae6d55f0530562895b57b
Levente Danel <danellevente@yahoo.com>
2019-08-17 19:49:51 +0300

    refactor for better readability

--- Queue.js
+++ Queue.js
@@ -1,29 +1,27 @@
 import Queue from 'better-queue'
 import rateLimit from 'function-rate-limit'
-import { MAX_REQ_PER_SECOND } from './constants'
-
-const RETRY_TIMEOUT = 100
+import { MAX_REQ_PER_SECOND, QUEUE_RETRY_TIMEOUT_MS } from './constants'
 
 export default class extends Queue {
   constructor(
     options = {
-      eventEmitter: null,
+      eventHandler: null,
       socket: null
     }
   ) {
     super(
       rateLimit(MAX_REQ_PER_SECOND, 1000, function({ message }, cb) {
-        options.socket.write(message, result => options.eventEmitter.emit('sent', message, result))
+        options.socket.write(message, result => options.eventHandler.emit('sent', message, result))
         cb(null, true)
       }),
       {
         precondition: cb => cb(null, options.socket.isConnected()),
-        preconditionRetryTimeout: RETRY_TIMEOUT,
+        preconditionRetryTimeout: QUEUE_RETRY_TIMEOUT_MS,
         priority: ({ func }, cb) =>
           ['sendClientVersion', 'sendClientId'].includes(func) ? cb(null, 10) : cb(null, 5)
       }
     )
     this._socket = options.socket
-    this._eventEmitter = options.eventEmitter
+    this._eventHandler = options.eventHandler
   }
 }
