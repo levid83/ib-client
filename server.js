daf63c602ff1356b80bdc7e36e31b9900bf0a7ad
Levente Danel <danellevente@yahoo.com>
2019-08-19 21:22:19 +0300

    add example order

--- server.js
+++ server.js
@@ -3,35 +3,35 @@ import http from 'http'
 import Socket from './src/Socket'
 import IBClient from './src'
 import ContractBuilder from './src/ContractBuilder'
-import Stock from './src/contracts/Stock'
+import OrderBuilder from './src/OrderBuilder'
 
 const port = 3000
 const server = http.createServer()
 
 const socket = new Socket({ host: '127.0.0.1', port: 5030 })
 const brokerClient = new IBClient({ socket: socket, clientId: 0 })
-
-brokerClient.on('nextValidId', id => console.log('nextValidId ', id))
-
+brokerClient.on('error', err =>
+  console.log('error id: ', err.id, ' code: ', err.code, ' message: ', err.message)
+)
 brokerClient.connect()
-brokerClient.reqIds(1)
 
 const contract = new ContractBuilder()
   .setSymbol('AAPL')
   .setSecType('STK')
+  .setExchange('NYSE')
   .build()
 
-const contract2 = new ContractBuilder()
-  .setSymbol('CL')
-  .setSecType('FUT')
-  .setExpiry('2019-10')
-  .setStrike(60.5)
-  .setRight('PUT')
-  .setMultiplier('1000')
+const order = new OrderBuilder()
+  .setAccount('DU535418')
+  .setAction('BUY')
+  .setQuantity(1)
+  .setOrderType('LMT')
+  .setLimitPrice(200)
   .build()
+
+brokerClient.on('nextValidId', id => brokerClient.placeOrder(id, contract, order))
 
 server.on('error', onError)
-
 server.listen(port, () => console.log(`server is listening on port ${port}`))
 
 function onError(error) {
