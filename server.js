da121f5ec38135b541dce0e5621ada6487219204
Levente Danel <danellevente@yahoo.com>
2019-08-20 11:12:36 +0300

    change the client example

--- server.js
+++ server.js
@@ -1,15 +1,11 @@
 import http from 'http'
-
-import Socket from './src/Socket'
-import IBClient from './src'
-import ContractBuilder from './src/ContractBuilder'
-import OrderBuilder from './src/OrderBuilder'
+import IBClient, { ContractBuilder, OrderBuilder } from './src' // ib-client
 
 const port = 3000
 const server = http.createServer()
 
-const socket = new Socket({ host: '127.0.0.1', port: 5030 })
-const brokerClient = new IBClient({ socket: socket, clientId: 0 })
+const brokerClient = new IBClient({ socket: { host: '127.0.0.1', port: 5030 }, clientId: 0 })
+
 brokerClient.on('error', err =>
   console.log('error id: ', err.id, ' code: ', err.code, ' message: ', err.message)
 )
